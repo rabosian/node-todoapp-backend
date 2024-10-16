662cfcac20cb5d04969f9d1372be4111f325e875
rabosian <msann1356@gmail.com>
2024-10-16 17:51:32 -0400

    DB connection updated

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors")
+const cors = require("cors");
+require("dotenv").config();
 const bodyParser = require("body-parser");
-const indexRouter = require("./routes/index")
+const indexRouter = require("./routes/index");
 
-const PORT_NUMBER = 4000
+const PORT_NUMBER = 4000;
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json()); // map request to req.body
 
-app.use('/api', indexRouter) // will concatenate /api in front of router path e.g. /tasks -> /api/tasks
+app.use("/api", indexRouter); // will concatenate /api in front of router path e.g. /tasks -> /api/tasks
 
-const mongoURI = `mongodb://localhost:27017/todoList`;
+const mongoURI = process.env.DB_URI;
 
 mongoose
   .connect(mongoURI)
   .then(() => console.log("DB connected"))
-  .catch((err) => console.log("DB connection failed"));
+  .catch((err) => console.log("DB connection failed: ", err));
 
 app.listen(PORT_NUMBER, () => {
-    console.log("server running on " + PORT_NUMBER)
-})
+  console.log("server running on " + PORT_NUMBER);
+});
